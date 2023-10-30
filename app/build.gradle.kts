d446ec196214cfd499418e87da41805caf49d11b
Ridhatama <ridhatama123@gmail.com>
2023-10-30 14:15:17 +0700

    fix bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,8 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
 
+    debugImplementation ("com.squareup.leakcanary:leakcanary-android:2.10")
+
 
 
 }
